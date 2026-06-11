b192515b2529ec89ff066acb39f1e8097313b3da
TTAWDTT <2451989@tongji.edu.cn>
2026-06-11 16:00:16 +0800

    chore: about微调

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -31,7 +31,7 @@ export default function DocsPage() {
                   <li>logo由Ziyi Zhuang原创，且并未借助AI手段，请勿随意转载/搬迁</li>
                   <li>[学院与专业]、[校区与宿舍]两部分的文字内容属于对应部分的作者，若转载/引用，请直接联系或通过站长联系对应的作者，并挂上此站链接</li>
                   <li>[学业][生活]部分的内容为大家合作完成，若需要转载/引用，请联系站长，并挂上此站链接</li>
-                  <li>该web page由TTAWDTT构建，如果您喜欢该设计，并希望用于您自己的内容，请在GitHub repo中鸣谢</li>
+                  <li>该web page由Zhen Luo（即TTAWDTT，站长）构建，如果您喜欢该设计，并希望用于您自己的内容，请在GitHub repo中鸣谢</li>
                 </ul>
               </Alert.Description>
             </Alert.Content>
